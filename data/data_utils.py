e731f7026251b81a2f1f4cd6c7fcbf43c421d58a
Shuang Liu <shuangliu@Shuangs-MacBook-Air.local>
2024-02-02 21:49:28 +0800

    feat: add more augmentations

--- data/data_utils.py
+++ data/data_utils.py
@@ -1,14 +1,26 @@
 
 def rescale(images, labels):
     # randomly rescale the image and return the augmented images + labels
-    raise NotImplemented
+    raise NotImplementedError
 
 
 def shift(images, labels):
-    raise NotImplemented
+    raise NotImplementedError
 
 
-def data_augmentation(images, labels, options=["rescaling", "shifting", "rotation", "saturation", "random noise"]):
+def rotation(images, labels):
+    raise NotImplementedError
+
+
+def saturation(images, labels):
+    raise NotImplementedError
+
+
+def noise(images, labels):
+    raise NotImplementedError
+
+
+def data_augmentation(images, labels, options=["rescaling", "shifting", "rotation", "saturation", "noise"]):
     """
     Data Augmentation Main Function. Augmentations can be chosen from:
     - Rescaling
@@ -19,8 +31,18 @@ def data_augmentation(images, labels, options=["rescaling", "shifting", "rotatio
     """
 
     for augment_option in options:
-        assert augment_option in ["rescaling", "shifting", "rotation", "saturation", "random noise"]
+        assert augment_option in ["rescaling", "shifting", "rotation", "saturation", "noise"]
         if augment_option == "rescaling":
             images, labels = rescale(images, labels)
         elif augment_option == "shifting":
             images, labels = shift(images, labels)
+        elif augment_option == "rotation":
+            images, labels = rotation(images, labels)
+        elif augment_option == "saturation":
+            images, labels = saturation(images, labels)
+        elif augment_option == "noise":
+            images, labels = noise(images, labels)
+        else:
+            raise ValueError(f"Undefined augmentation option: {augment_option}")
+
+    return images, labels
